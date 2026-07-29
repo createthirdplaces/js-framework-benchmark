79e4c8c9c13035d8398f007b14244d1848c89f23
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-07-29 02:14:51 -0400

    Updates

--- frameworks/non-keyed/places-js/src/main.js
+++ frameworks/non-keyed/places-js/src/main.js
@@ -1,4 +1,4 @@
-import {BaseDynamicComponent,DataStore} from "/lib/places-js-latest.js";
+import {BaseDynamicComponent,CustomLoadAction,DataStore} from "./lib/places-js-latest.js";
 
 function _random(max) {
     return Math.round(Math.random()*1000)%max;
@@ -6,6 +6,7 @@ function _random(max) {
 
 class Store extends DataStore {
 
+   id=1;
    buildData(count = 1000) {
         var adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
         var colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
@@ -13,34 +14,37 @@ class Store extends DataStore {
         var data = [];
         for (var i = 0; i < count; i++)
             data.push({id: this.id++, label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)] });
-        return data;
+      return data;
     }
     
     updateData(mod = 10) {
         // Just assigning setting each tenth this.data doesn't cause a redraw, the following does:
-        var newData = [...this.data];
+        var newData = [...this.getStoreData().data];
 
         for (let i = 0; i < newData.length; i += 10) {
-            newData[i].label += ' !!!';
+            newData[i] = {...newData[i], label:newData[i].label + '!!!'};
         }
         this.updateStoreData({data:newData});
     }
     
     delete(id) {
-        const idx = this.data.findIndex(d => d.id==id);
-        this.updatStoreData({data: this.data.slice(0, idx).concat(this.data.slice(idx + 1)));
+        const data = [...this.getStoreData().data];
+        const idx = data.findIndex(d => d.id==id);
+        this.updateStoreData({
+          data: data.slice(0, idx).concat(data.slice(idx + 1))
+        });
     }
         
     run() {
         this.updateStoreData({
-          data:this.buildData,
+          data:this.buildData(),
           selected: undefined
         });
     }
     
     add() {
         this.updateStoreData({
-          data: this.data.concat(this.buildData(1000));
+          data: this.getStoreData().data.concat(this.buildData(1000))
         });
     }
     
@@ -49,6 +53,7 @@ class Store extends DataStore {
     }
     
     select(id) {
+
       this.updateStoreData({
         selected:id
       });
@@ -64,11 +69,11 @@ class Store extends DataStore {
         this.updateStoreData({
           data:[],
           selected:undefined
-        );
+        });
     }
     
     swapRows() {
-      let data = this.getStoreData;
+      let data = [...this.getStoreData().data];
 
       if(data.length > 998) {
         let d1 = data[1];
@@ -83,7 +88,7 @@ class Store extends DataStore {
           }
           return data2;
         });
-        this..updateStoreData({
+        this.updateStoreData({
           data:newData
         });
       }
@@ -91,7 +96,7 @@ class Store extends DataStore {
 
 }
 
-setData = function() {
+const setData = function() {
   return {
     data:[],
     selected: undefined,
@@ -103,56 +108,86 @@ const store = new Store(new CustomLoadAction(setData));
 
 export class MainElement extends BaseDynamicComponent {
   constructor(){
-    super({
-      dataStoreSubscriptions:[{
-        dataStore:Store
+    super([{
+        dataStore:store
       }]
-    });
+    );
     
     const self = this;
     this.addEventListener("click", e=> {
-        const { action, id } = e.target.dataset
-        if (action && id) {
-            self['_' + action](id)
-        }
+          e.preventDefault();
+          if (e.target.id === 'add') {
+                this.add();
+            }
+            else if (e.target.id === 'run') {
+                this.run();
+            }
+            else if (e.target.id === 'update') {
+                this.update();
+            }
+            else if (e.target.id === 'hideall') {
+                this.hideAll();
+            }
+            else if (e.target.id === 'showall') {
+                this.showAll();
+            }
+            else if (e.target.id === 'runlots') {
+                this.runLots();
+            }
+            else if (e.target.id === 'clear') {
+                this.clear();
+            }
+            else if (e.target.id === 'swaprows') {
+                this.swapRows();
+            }
+            else if (e.target.dataset.action === 'remove') {
+                this.remove(e.target.dataset.id);
+            }
+            else if (e.target.dataset.action === 'select') {
+                this.select(e.target.dataset.id);
+            } 
+
     });
   }
 
+ 
+  
   render(data){
-    return `<link href="/css/currentStyle.css" rel="stylesheet"/>
+    const rows = data.data;
+    const html = `<link href="/css/currentStyle.css" rel="stylesheet"/>
         <div class="container">
             <div class="jumbotron">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>Lit non-keyed</h1>
+                        <h1>Places.js non-keyed</h1>
                     </div>
                     <div class="col-md-6">
                         <div class="row">
                             <div class="col-sm-6 smallpad">
-                            <button type="button" class="btn btn-primary btn-block" id="run" @click=${this._run}>Create 1,000 rows</button>
+                            <button type="button" class="btn btn-primary btn-block" id="run">Create 1,000 rows</button>
                             </div>
                             <div class="col-sm-6 smallpad">
-                                <button type="button" class="btn btn-primary btn-block" id="runlots" @click=${this._runLots}>Create 10,000 rows</button>
+                                <button type="button" class="btn btn-primary btn-block" id="runlots">Create 10,000 rows</button>
                             </div>
                             <div class="col-sm-6 smallpad">
-                                <button type="button" class="btn btn-primary btn-block" id="add" @click=${this._add}>Append 1,000 rows</button>
+                                <button type="button" class="btn btn-primary btn-block" id="add" >Append 1,000 rows</button>
                             </div>
                             <div class="col-sm-6 smallpad">
-                                <button type="button" class="btn btn-primary btn-block" id="update" @click=${this._update}>Update every 10th row</button>
+                                <button type="button" class="btn btn-primary btn-block" id="update">Update every 10th row</button>
                             </div>
                             <div class="col-sm-6 smallpad">
-                                <button type="button" class="btn btn-primary btn-block" id="clear" @click=${this._clear}>Clear</button>
+                                <button type="button" class="btn btn-primary btn-block" id="clear" >Clear</button>
                             </div>
                             <div class="col-sm-6 smallpad">
-                                <button type="button" class="btn btn-primary btn-block" id="swaprows" @click=${this._swapRows}>Swap Rows</button>
+                                <button type="button" class="btn btn-primary btn-block" id="swaprows">Swap Rows</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <table class="table table-hover table-striped test-data" @click=${this._handleClick}>
-                <tbody>${data.map(item => html`
-                <tr id=${item.id} class=${this._selected == item.id ? 'danger' : ''}>
+            <table class="table table-hover table-striped test-data" >
+                <tbody>${rows.map(item => `
+                <tr id=${item.id} class=${data.selected == item.id ? 'danger' : ''}>
                     <td class="col-md-1">${item.id}</td>
                     <td class="col-md-4">
                     <a data-action="select" data-id=${item.id}>${item.label}</a>
@@ -164,51 +199,38 @@ export class MainElement extends BaseDynamicComponent {
                     </a>
                     </td>
                     <td class="col-md-6"></td>
-                </tr>`)}
+                </tr>`).join("")}
                 </tbody>
             </table>
             <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true"></span>
-        </div>`
+        </div>`;
+        return html;
+    }
 
-  }
-
-    _add() {
+    add() {
         store.add();
-        this._sync();
     }
-    _remove(id) {
+    remove(id) {
         store.delete(id);
-        this._sync();
     }
-    _select(id) {
+    select(id) {
         store.select(id);
-        this._sync();
     }
-    _run() {
+    run() {
         store.run();
-        this._sync();
     }
-    _update() {
+    update() {
         store.update();
-        this._sync();
     }
-    _runLots() {
+    runLots() {
         store.runLots();
-        this._sync();
     }
-    _clear() {
+    clear() {
         store.clear();
-        this._sync();
     }
-    _swapRows() {
+    swapRows() {
         store.swapRows();
-        this._sync();
     }
-    _sync() {
-        this._rows = store.data;
-        this._selected = store.selected;
-    }
-
 }
 
 customElements.define("main-element", MainElement);
